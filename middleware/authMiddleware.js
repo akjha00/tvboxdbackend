60c015bfe59bc5948fd0f76ef7eb6e0ca2a45371
Abhaya Krishnan-Jha <abhayakrishnanjha@Abhayas-Air.lan>
2025-05-16 10:54:49 -0700

    logs

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,8 +1,11 @@
 const jwt = require('jsonwebtoken');
 
 function authenticateToken(req, res, next) {
+  console.log("at auth");
   const authHeader = req.headers['authorization'];
+  console.log(authHeader);
   const token = authHeader?.split(' ')[1];
+  console.log(token);
 
   if (!token) return res.sendStatus(401);
 
