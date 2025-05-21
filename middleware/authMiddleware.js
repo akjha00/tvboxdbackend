2ae12391835beb5dc57dd1fa709dc0b2bef8dc02
Abhaya Krishnan-Jha <abhayakrishnanjha@Abhayas-Air.lan>
2025-05-20 17:57:31 -0700

    main changes

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,10 +1,16 @@
 const jwt = require('jsonwebtoken');
 
 function authenticateToken(req, res, next) {
+  console.log("authenticate");
+  console.log(req.headers['authorization']);
   const authHeader = req.headers['authorization'];
   const token = authHeader?.split(' ')[1];
+  console.log(token);
 
-  if (!token) return res.sendStatus(401);
+  if (!token){
+    console.log("culprit");
+    return res.sendStatus(401);
+  }
 
   try {
     const user = jwt.verify(token, process.env.JWT_SECRET);
--- routes/users.js
+++ routes/users.js
@@ -57,6 +57,7 @@ try {
 // GET /api/users/me
 router.get('/me', authenticate, async (req, res) => {
   try {
+    console.log("id:" + req.user.userId);
     const result = await pool.query('SELECT id, username, email FROM users WHERE id = $1', [req.user.userId]);
     if (result.rows.length === 0) return res.status(404).json({ error: 'User not found' });
     res.json({ user: result.rows[0] });
